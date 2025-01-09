e84e849a4aa5e71a72582e2340f18102ed5ca870
kimhayeon <k.hayeon00@gmail.com>
2025-01-09 17:02:05 +0900

    MODU-90 feat: tokens.json -> 디자인 토큰 로직 수정

--- scripts/build-tokens.js
+++ scripts/build-tokens.js
@@ -3,27 +3,12 @@ import StyleDictionary from 'style-dictionary';
 
 register(StyleDictionary);
 
-const customTypographyTransform = {
-  name: 'custom/typography',
-  type: 'value',
-  transform: token => {
-    if (typeof token.original.value === 'object') {
-      return { css: token.original.value };
-    }
-
-    return token.original.value;
-  }
-};
-
-StyleDictionary.registerTransform(customTypographyTransform);
-
 const sd = new StyleDictionary({
   source: ['tokens/transform/*.json'],
   preprocessors: ['tokens-studio'],
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
-      transforms: ['custom/typography'],
       buildPath: 'tokens/build/',
       files: [
         {
