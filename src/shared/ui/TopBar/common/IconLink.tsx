2795bdeddffa57a89d25a8279e859377fc9904dd
kimhayeon <k.hayeon00@gmail.com>
2025-02-07 14:11:43 +0900

    style: 스타일 수정

--- src/shared/ui/TopBar/common/IconLink.tsx
+++ src/shared/ui/TopBar/common/IconLink.tsx
@@ -14,7 +14,7 @@ export default function IconLink({
   alt: string;
 }) {
   return (
-    <Link href={href} className={`w-auto shrink-0 p-1.5 ${className}`}>
+    <Link href={href} className={`shrink-0 p-1.5 ${className}`}>
       <Icon src={src} alt={alt} />
     </Link>
   );
