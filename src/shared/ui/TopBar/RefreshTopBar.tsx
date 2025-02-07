c572e6cc95c9ae1f520562a441fa8168cff4da8f
kimhayeon <k.hayeon00@gmail.com>
2025-02-07 14:08:01 +0900

    fix: BackLink -> BackBtn

--- src/shared/ui/TopBar/RefreshTopBar.tsx
+++ src/shared/ui/TopBar/RefreshTopBar.tsx
@@ -1,11 +1,11 @@
-import { Header, Title, BackLink, LanguageBtn, IconBtn } from './common';
+import { Header, Title, BackBtn, LanguageBtn, IconBtn } from './common';
 
 export default function RefreshTopBar({ title, refresh }: { title?: string; refresh: () => void }) {
   return (
     <Header>
       <h1 className="sr-only">MODU</h1>
       {title && <Title title={title} />}
-      <BackLink />
+      <BackBtn />
       <LanguageBtn className="ml-auto" />
       <IconBtn onClick={refresh} src="/svgs/refresh.svg" alt="refresh" />
     </Header>
